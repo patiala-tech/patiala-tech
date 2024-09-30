c0d219cd089f8d81e1f041f9840c815a8a6e6d66
patiala-tech <patiala91176@gmail.com>
2024-10-01 01:07:15 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,19 @@
+import streamlit as st
+st.title("Testing connection")
+
+
+
+st.image("./media/img1.jpg")
+st.write("display video")
+video_file = open('Creative202.mp4', 'rb') #enter the filename with filepath
+
+video_bytes = video_file.read() #reading the file
+
+st.video(video_bytes) #displaying the video
+
+
+
+
+
+
+import mysql.connector
